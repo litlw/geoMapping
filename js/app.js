2cfa72bfc6f02d6fbf60186eeedb5c0207f36295
litlw <hasaunderritchie@gmail.com>
2017-06-17 20:03:58 -0400

    Starting actual work lolol

---
+++ js/app.js
@@ -0,0 +1,1 @@
+var app = angular.module("geo_mapping_tool", [ 'ngAria', 'ngAnimate', 'ngMaterial']);
---
+++ js/controllers/MainController.js
@@ -0,0 +1,25 @@
+//This is only the main controller. the other controllers can be found in the modules section.
+
+app.controller("mainController", function($scope) {
+  // this is where you will find global variables and controllers.
+
+  $scope.locations = [{
+    name: "Toronto",
+    src: "src/toronto_geoJSON.js",
+    data_sources: [] //some source here
+  },{
+    name: "New York",
+    src: "src/NewYorkbyNeighbourhood.js",
+    data_sources: [{
+      name: "All Outputs",
+      src: "src/output.csv"
+    }, {
+      name: "Dropoff Outputs",
+      src: "src/output_dropoffs.csv"
+    },{
+      name: "Source Data",
+      src: "src/locations.csv"
+    }] //some source here
+  }];
+$scope.helloWorld = "Hello World. I just wanted to let you know that I think you are a huge plunker. fuck man. ";
+});
